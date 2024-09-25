e29c8c5b522c164539b9a8fb2e367b08bb88390c
Vlad <vladislav.95antonov@gmail.com>
2024-09-25 23:01:38 +0300

    fix tea and tableware section and adaptive

--- src/sections/DetailedTableware/DetailedTableware.jsx
+++ src/sections/DetailedTableware/DetailedTableware.jsx
@@ -13,32 +13,34 @@ import { tablewares } from "./tableware";
 // create details tableware component
 const DetailedTableware = ({ onBack }) => {
   return (
-    <section className="container m-auto max-w-[280px] bigMobile:max-w-[400px] tablet:max-w-[768px] laptop:max-w-[1024px]">
+    <section className="container m-auto">
       <section className="flex py-8">
         <button onClick={onBack}>
           <IoIosArrowBack className="h-10 w-10 text-wood" />
         </button>
-        <h2 className="mx-auto text-2xl font-bold">Наявний Посуд</h2>
+        <h2 className="mx-auto text-2xl font-bold">Наявний посуд</h2>
       </section>
-      <section>
+      <section className="mx-auto max-w-[700px]">
         {tablewares.map((tableware) => (
           <article
-            className="mb-8 flex rounded-3xl p-8 shadow-custom"
+            className="mb-4 flex flex-col rounded-3xl p-4 text-center shadow-custom tablet:mb-8 tablet:flex-row tablet:p-8"
             key={tableware.id}
           >
             <Image
               src={tableware.imageUrl}
-              alt={`tea ${tableware.name}`}
+              alt={`tableware ${tableware.name}`}
               priority={true}
-              width={400}
-              height={300}
-              className="h-[300px] w-[400px] rounded-[12px]"
+              width={500}
+              height={500}
+              className="h-[100%] w-[400px] rounded-[12px] tablet:mr-8 tablet:min-w-[256px]"
             />
-            <section className="ml-8 max-w-[500px]">
-              <h3 className="my-8 text-lg">{tableware.name}</h3>
+            <section className="max-w-[500px]">
+              <h3 className="my-4 text-l tablet:my-8 tablet:text-lg">
+                {tableware.name}
+              </h3>
               <p className="text-m">{tableware.description}</p>
               <Button
-                className="mt-8 h-[50px] w-[50%] text-m font-semibold"
+                className="mt-4 h-[50px] w-[50%] text-m font-semibold tablet:mt-8"
                 title="Зробити замовленя чаю"
               >
                 {`Замовити`}
--- src/sections/DetailedTea/DetailedTea.jsx
+++ src/sections/DetailedTea/DetailedTea.jsx
@@ -13,32 +13,34 @@ import { teas } from "../Tea/teas";
 // create DatailedTea component
 const DetailedTea = ({ onBack }) => {
   return (
-    <section className="container m-auto max-w-[280px] bigMobile:max-w-[400px] tablet:max-w-[768px] laptop:max-w-[1024px]">
+    <section className="container m-auto">
       <section className="flex py-8">
         <button onClick={onBack}>
           <IoIosArrowBack className="h-10 w-10 text-wood" />
         </button>
         <h2 className="mx-auto text-2xl font-bold">Наявні Чаї</h2>
       </section>
-      <section>
+      <section className="mx-auto max-w-[700px]">
         {teas.map((tea) => (
           <article
-            className="mb-8 flex rounded-3xl p-8 shadow-custom"
+            className="mb-4 flex flex-col rounded-3xl p-4 text-center shadow-custom tablet:mb-8 tablet:flex-row tablet:p-8"
             key={tea.id}
           >
             <Image
               src={tea.imageUrl}
               alt={`tea ${tea.name}`}
               priority={true}
-              width={400}
-              height={300}
-              className="h-[300px] w-[400px] rounded-[12px]"
+              width={500}
+              height={500}
+              className="h-[100%] min-h-[348px] w-[400px] rounded-[12px] tablet:mr-8 tablet:min-w-[256px]"
             />
-            <section className="ml-8 max-w-[500px]">
-              <h3 className="my-8 text-lg">{tea.name}</h3>
+            <section className="max-w-[500px]">
+              <h3 className="my-4 text-l tablet:my-8 tablet:text-lg">
+                {tea.name}
+              </h3>
               <p className="text-m">{tea.bigDescription}</p>
               <Button
-                className="mt-8 h-[50px] w-[50%] text-m font-semibold"
+                className="mt-4 h-[50px] w-[50%] text-m font-semibold tablet:mt-8"
                 title="Зробити замовленя чаю"
               >
                 {`Замовити`}
