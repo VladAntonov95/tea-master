ae96dc16e13245847b7b37ef31b275f3e125b8d1
Vlad <vladislav.95antonov@gmail.com>
2024-10-16 14:08:54 +0300

    fix

--- src/sections/Cart/Cart.jsx
+++ src/sections/Cart/Cart.jsx
@@ -1,11 +1,23 @@
 "use client";
 import React, { useState } from "react";
 import { useCart } from "@/context/CartContext";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import Button from "@/components/Button/Button";
 import Order from "../Order/Order";
+import Modal from "@/components/Modal/Modal";
+
+const schema = Yup.object().shape({
+  customerName: Yup.string().required("Введіть ваше імʼя"),
+  customerPhone: Yup.string()
+    .required("Введіть ваш номер телефону")
+    .matches(/^\+?\d{10,12}$/, "Некоректний формат телефону"),
+});
 
 const Cart = ({ onBack, removeFromCart, clearCart }) => {
   const [isOrderSuccess, setIsOrderSuccess] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
   const {
     cartItems,
     customerName,
@@ -14,14 +26,17 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
     setCustomerPhone,
   } = useCart();
 
-  const handleOrder = async () => {
-    if (!customerName || !customerPhone) {
-      alert("Будь-ласка заповніть усі поля.");
-      return;
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
+  const handleOrder = async (data) => {
     const orderText =
-      `Покупець: ${customerName}\nТелефон: ${customerPhone}\n\nЗамовлення:\n` +
+      `Покупець: ${data.customerName}\nТелефон: ${data.customerPhone}\n\nЗамовлення:\n` +
       cartItems
         .map((item) => {
           return item.name === "Доєднатись до школи"
@@ -30,9 +45,9 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
         })
         .join("\n");
 
-    const TELEGRAM_API_URL = `https://api.telegram.org/bot7842960073:AAHvBohpeeTfI5symdUwLUZbTrJZBM0dycU/sendMessage`;
+    const TELEGRAM_API_URL = `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN}/sendMessage`;
 
-    const chatIds = ["404735563", "175599878"];
+    const chatIds = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_IDS.split(",");
 
     try {
       for (const chatId of chatIds) {
@@ -61,7 +76,7 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
       setIsOrderSuccess(true);
     } catch (error) {
       console.error("Помилка", error);
-      alert("Упс! Виникла помилка при відправлені заказу");
+      setModalMessage("Упс! Виникла помилка при відправлені заказу");
     }
   };
 
@@ -71,6 +86,10 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
 
   return (
     <section className="container">
+      {modalMessage && (
+        <Modal onClose={() => setModalMessage("")}>{modalMessage}</Modal>
+      )}
+
       <section className="relative mx-auto mt-4 max-w-[750px] items-center rounded-t-2xl bg-background text-center shadow-custom tablet:mt-16">
         <h2 className="py-4 font-merriweather text-lg font-bold">
           Ваше замовлення
@@ -79,19 +98,28 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
           <input
             type="text"
             placeholder="Ваше імʼя"
+            {...register("customerName")}
             value={customerName}
             onChange={(e) => setCustomerName(e.target.value)}
             className="border-receipt mb-2 w-full rounded-md border p-2"
           />
+          {errors.customerName && (
+            <p className="text-red-500">{errors.customerName.message}</p>
+          )}
           <input
             type="text"
             placeholder="Ваш телефон"
+            {...register("customerPhone")}
             value={customerPhone}
             onChange={(e) => setCustomerPhone(e.target.value)}
             className="border-receipt w-full rounded-md border p-2"
           />
+          {errors.customerPhone && (
+            <p className="text-red-500">{errors.customerPhone.message}</p>
+          )}
         </div>
       </section>
+
       <section className="ticket bg-receipt relative mx-auto mb-4 max-w-[750px] py-4 text-center shadow-custom tablet:mb-16">
         {cartItems.length > 0 ? (
           <ul className="mb-4 font-roboto text-m">
@@ -119,7 +147,7 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
         </Button>
         <Button
           className="mx-auto mt-4 w-[231px] px-[72px] py-2 font-roboto text-md font-semibold tablet:mt-0"
-          onClick={handleOrder}
+          onClick={handleSubmit(handleOrder)} // Используем handleSubmit для отправки формы
         >
           Замовити
         </Button>
