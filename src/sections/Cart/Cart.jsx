875fa7e844a3099c8cc7d10acc4143c61e898192
Vlad <vladislav.95antonov@gmail.com>
2024-10-18 19:07:57 +0300

    add loader, add validation

--- src/sections/Cart/Cart.jsx
+++ src/sections/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useCart } from "@/context/CartContext";
 import { useForm } from "react-hook-form";
 import * as Yup from "yup";
@@ -9,25 +9,55 @@ import Modal from "@/components/Modal/Modal";
 import { GiEmptyWoodBucket } from "react-icons/gi";
 
 const schema = Yup.object().shape({
-  customerName: Yup.string().required("Введіть ваше імʼя"),
+  customerName: Yup.string()
+    .required("Введіть ваше імʼя")
+    .min(2, "Ім'я має містити мінімум 2 символи")
+    .max(50, "Ім'я не може бути довшим за 50 символів")
+    .matches(
+      /^[A-Za-zА-Яа-яЁёЇїІіЄєҐґ' ]+$/,
+      "Ім'я має містити тільки літери і не може містити цифри",
+    ),
   customerPhone: Yup.string()
     .required("Введіть ваш номер телефону")
-    .matches(/^\+?\d{10,12}$/, "Некоректний формат телефону"),
-  customerEmail: Yup.string().required("Введіть ваш e-mail"),
+    .matches(
+      /^\+?\d{10,12}$/,
+      "Некоректний формат телефону. Номер має містити від 10 до 12 цифр і може починатися з '+'",
+    ),
+  customerEmail: Yup.string()
+    .required("Введіть ваш e-mail")
+    .email("Некоректний формат e-mail"),
 });
 
 const Cart = ({ onBack, removeFromCart, clearCart }) => {
   const [isOrderSuccess, setIsOrderSuccess] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
-  const { cartItems, updateCartItemQuantity } = useCart();
+  const [loading, setLoading] = useState(false);
+
+  const {
+    cartItems,
+    setCustomerName,
+    setCustomerPhone,
+    setCustomerEmail,
+    customerName,
+    customerPhone,
+    customerEmail,
+    updateCartItemQuantity,
+  } = useCart();
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm({
     resolver: yupResolver(schema),
   });
+
+  useEffect(() => {
+    setValue("customerName", customerName);
+    setValue("customerPhone", customerPhone);
+    setValue("customerEmail", customerEmail);
+  }, [customerName, customerPhone, customerEmail, setValue]);
 
   const handleOrder = async (data) => {
     if (!cartItems || cartItems.length === 0) {
@@ -39,6 +69,8 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
       ...data,
       cartItems: cartItems,
     };
+
+    setLoading(true);
 
     try {
       const response = await fetch("/api/SendOrder", {
@@ -55,27 +87,25 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
       }
 
       const result = await response.json();
-      console.log(result.message);
       setIsOrderSuccess(true);
       clearCart();
     } catch (error) {
-      console.error("Помилка:", error);
       setModalMessage(
         error.message || "Упс! Виникла помилка при відправлені заказу",
       );
-    }
-  };
-
-  const handleQuantityChange = (cartId, quantity) => {
-    if (quantity < 1) {
-      removeFromCart(cartId);
-    } else {
-      updateCartItemQuantity(cartId, quantity);
+    } finally {
+      setLoading(false);
     }
   };
 
   if (isOrderSuccess) {
-    return <Order onBack={onBack} />;
+    return (
+      <Order
+        onBack={onBack}
+        text=" Ваше замовлення успішно відправлено. Ми зв&#039;яжемося з вами найближчим часом."
+        showButton={true}
+      />
+    );
   }
 
   return (
@@ -84,119 +114,128 @@ const Cart = ({ onBack, removeFromCart, clearCart }) => {
         <Modal message={modalMessage} onClose={() => setModalMessage("")} />
       )}
 
-      <section className="relative mx-auto mt-4 max-w-[750px] items-center rounded-t-2xl bg-background text-center shadow-custom tablet:mt-16">
-        <h2 className="py-4 font-merriweather text-lg font-bold">
-          Ваше замовлення
-        </h2>
-        <div className="mx-auto max-w-[500px] pb-8">
-          <input
-            type="text"
-            placeholder="Ваше імʼя"
-            {...register("customerName")}
-            className={`border-receipt mb-2 w-full rounded-md border p-2 ${errors.customerName ? "border-error" : ""}`}
-          />
-          {errors.customerName && (
-            <p className="text-error">{errors.customerName.message}</p>
-          )}
+      {loading ? (
+        <Order text="Ваш запит обробляється" showButton={false} />
+      ) : (
+        <>
+          <section className="relative mx-auto mt-4 max-w-[750px] items-center rounded-t-2xl bg-background text-center shadow-custom tablet:mt-16">
+            <h2 className="py-4 font-merriweather text-lg font-bold">
+              Ваше замовлення
+            </h2>
+            <div className="mx-auto max-w-[500px] pb-8">
+              <input
+                type="text"
+                placeholder="Ваше імʼя"
+                {...register("customerName")}
+                onChange={(e) => {
+                  setCustomerName(e.target.value);
+                }}
+                className={`border-receipt mb-2 w-full rounded-md border p-2 ${errors.customerName ? "border-error" : ""}`}
+              />
+              {errors.customerName && (
+                <p className="text-error">{errors.customerName.message}</p>
+              )}
 
-          <input
-            type="text"
-            placeholder="Ваш телефон"
-            {...register("customerPhone")}
-            className={`border-receipt mb-2 w-full rounded-md border p-2 ${errors.customerPhone ? "border-error" : ""}`}
-          />
-          {errors.customerPhone && (
-            <p className="text-error">{errors.customerPhone.message}</p>
-          )}
+              <input
+                type="text"
+                placeholder="Ваш телефон"
+                {...register("customerPhone")}
+                onChange={(e) => {
+                  setCustomerPhone(e.target.value);
+                }}
+                className={`border-receipt mb-2 w-full rounded-md border p-2 ${errors.customerPhone ? "border-error" : ""}`}
+              />
+              {errors.customerPhone && (
+                <p className="text-error">{errors.customerPhone.message}</p>
+              )}
 
-          <input
-            type="email"
-            placeholder="Ваш e-mail"
-            {...register("customerEmail")}
-            className={`border-receipt w-full rounded-md border p-2 ${errors.customerEmail ? "border-error" : ""}`}
-          />
-          {errors.customerEmail && (
-            <p className="text-error">{errors.customerEmail.message}</p>
-          )}
-        </div>
-      </section>
+              <input
+                type="email"
+                placeholder="Ваш e-mail"
+                {...register("customerEmail")}
+                onChange={(e) => {
+                  setCustomerEmail(e.target.value);
+                }}
+                className={`border-receipt w-full rounded-md border p-2 ${errors.customerEmail ? "border-error" : ""}`}
+              />
+              {errors.customerEmail && (
+                <p className="text-error">{errors.customerEmail.message}</p>
+              )}
+            </div>
+          </section>
 
-      <section className="ticket bg-receipt relative mx-auto mb-4 max-w-[750px] pb-4 text-center shadow-custom tablet:mb-16">
-        {cartItems.length > 0 ? (
-          <ul className="mx-4 mb-4 items-center font-roboto text-m">
-            {cartItems.map((item) => (
-              <li
-                key={item.cartId}
-                className="flex min-h-[50px] flex-row items-center justify-between border-b border-b-black-text text-s bigMobile:p-2 tablet:text-sm"
-              >
-                <p className="w-[120px] max-w-[120px] text-left tablet:min-w-[290px]">
-                  <strong>{item.name}</strong>
-                </p>
-                <p>
-                  {item.price ? ` ${item.price * item.quantity} грн` : null}
-                </p>
-                {item.name !== "Доєднатись до школи" ? (
-                  <div className="flex items-center">
-                    <div>
+          <section className="ticket bg-receipt relative mx-auto mb-4 max-w-[750px] pb-4 text-center shadow-custom tablet:mb-16">
+            {cartItems.length > 0 ? (
+              <ul className="mx-4 mb-4 items-center font-roboto text-m">
+                {cartItems.map((item) => (
+                  <li
+                    key={item.cartId}
+                    className="flex min-h-[50px] flex-col items-center justify-between border-b border-b-black-text text-s bigMobile:p-2 tablet:flex-row tablet:text-sm"
+                  >
+                    <p className="text-left tablet:min-w-[290px]">
+                      <strong>{item.name}</strong>
+                    </p>
+                    <p className="w-24 text-center">
+                      {item.price ? `${item.price * item.quantity} грн` : null}
+                    </p>
+                    <div className="flex flex-wrap items-center space-x-2">
                       <button
-                        className="rounded-full text-center tablet:h-[30px] tablet:w-[30px] tablet:border"
+                        className="h-[30px] w-[30px] rounded-full border text-center"
                         onClick={() =>
-                          handleQuantityChange(item.cartId, item.quantity - 1)
+                          updateCartItemQuantity(item.cartId, item.quantity - 1)
                         }
                       >
                         <span>-</span>
                       </button>
                       <input
-                        type="text"
+                        type="number"
                         min="1"
                         value={item.quantity || 1}
                         onChange={(e) => {
                           const value = Math.max(1, Number(e.target.value));
-                          handleQuantityChange(item.cartId, value);
+                          updateCartItemQuantity(item.cartId, value);
                         }}
-                        className="bg-receipt w-[40px] rounded text-center mobile:w-[50px] bigMobile:mx-2"
+                        className="bg-receipt w-[30px] rounded text-center"
                       />
                       <button
-                        className="rounded-full text-center tablet:h-[30px] tablet:w-[30px] tablet:border"
+                        className="h-[30px] w-[30px] rounded-full border text-center"
                         onClick={() =>
-                          handleQuantityChange(item.cartId, item.quantity + 1)
+                          updateCartItemQuantity(item.cartId, item.quantity + 1)
                         }
                       >
                         <span>+</span>
                       </button>
+                      <button
+                        className="mb-2"
+                        onClick={() => removeFromCart(item.cartId)}
+                      >
+                        <GiEmptyWoodBucket className="h-9 w-9" />
+                      </button>
                     </div>
-                    <button
-                      className="ml-2"
-                      onClick={() => removeFromCart(item.cartId)}
-                    >
-                      <GiEmptyWoodBucket className="mb-2 h-9 w-9" />
-                    </button>
-                  </div>
-                ) : (
-                  <button onClick={() => removeFromCart(item.cartId)}>
-                    <GiEmptyWoodBucket className="h-9 w-9" />
-                  </button>
-                )}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p className="mb-4 font-roboto text-m">Очікуємо на ваше замовлення</p>
-        )}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="py-4 font-roboto text-m">
+                Очікуємо на ваше замовлення
+              </p>
+            )}
 
-        <Button
-          onClick={onBack}
-          className="mx-auto w-[231px] px-[72px] py-2 font-roboto text-md font-semibold tablet:mr-4"
-        >
-          Назад
-        </Button>
-        <Button
-          className="mx-auto mt-4 w-[231px] px-[72px] py-2 font-roboto text-md font-semibold tablet:mt-0"
-          onClick={handleSubmit(handleOrder)}
-        >
-          Замовити
-        </Button>
-      </section>
+            <Button
+              onClick={onBack}
+              className="mx-auto w-[231px] px-[72px] py-2 font-roboto text-md font-semibold tablet:mr-4"
+            >
+              Назад
+            </Button>
+            <Button
+              className="mx-auto mt-4 w-[231px] px-[72px] py-2 font-roboto text-md font-semibold tablet:mt-0"
+              onClick={handleSubmit(handleOrder)}
+            >
+              Підтвердити
+            </Button>
+          </section>
+        </>
+      )}
     </section>
   );
 };
--- src/sections/Order/Order.jsx
+++ src/sections/Order/Order.jsx
@@ -2,23 +2,22 @@
 import React from "react";
 import Button from "@/components/Button/Button";
 
-const OrderConfirmation = ({ onBack }) => {
+const OrderConfirmation = ({ onBack, text, showButton = true }) => {
   return (
     <section className="container flex min-h-[450px] items-center justify-center">
       <section className="relative max-w-[750px] rounded-2xl bg-background px-8 text-center shadow-custom">
         <h2 className="py-8 font-merriweather text-lg font-bold">
           Дякуємо за замовлення!
         </h2>
-        <p className="mx-auto max-w-[500px] font-roboto text-m">
-          Ваше замовлення успішно відправлено. Ми зв&#039;яжемося з вами
-          найближчим часом.
-        </p>
-        <Button
-          onClick={onBack}
-          className="mx-auto mb-8 mt-8 w-[231px] px-[72px] py-2 font-roboto text-md font-semibold"
-        >
-          Назад
-        </Button>
+        <p className="mx-auto mb-8 max-w-[500px] font-roboto text-m">{text}</p>
+        {showButton && (
+          <Button
+            onClick={onBack}
+            className="mx-auto mb-8 w-[231px] px-[72px] py-2 font-roboto text-md font-semibold"
+          >
+            Назад
+          </Button>
+        )}
       </section>
     </section>
   );
